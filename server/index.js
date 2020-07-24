330c5b46cd9c51fd0022f42a4bd8fd1a5ff5bcb7
Joshua Morris <25271173+jmorris1990@users.noreply.github.com>
2020-07-24 17:12:12 -0400

    remove prod check

--- server/index.js
+++ server/index.js
@@ -11,11 +11,9 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-    app.get(/.*/, (req, res) => res.sendfile(__dirname + '/public/index.html'));
-}
+app.get(/.*/, (req, res) => res.sendfile(__dirname + '/public/index.html'));
 
 const port = 5000;
 
